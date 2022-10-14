f2086ad0cec3b714c2557df4cbe3c6e80848e9d0
fahmi ahmad <fahmiahmad@tutanota.com>
2022-10-14 22:30:12 +0700

    cylon eyes

---
+++ bare-metal/cylon_eyes/cylon_eyes.c
@@ -0,0 +1,37 @@
+#define __AVR_ATmega328P__
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DELAYTIME 500
+#define LED_PORT PORTB
+#define LED_PIN PINB
+#define LED_DDR DDRB
+
+int main(void)
+{
+    uint8_t i = 0;
+    // set up all of LED pins for output
+    DDRB = 0xff;
+
+    while (1)
+    {
+        while (i < 7)
+        {
+            // illuminate only i'th pin
+            LED_PORT = (1 << i);
+            _delay_ms(DELAYTIME);
+            // next LED
+            i++;
+        }
+
+        while (i > 0)
+        {
+            LED_PORT = (1 << i);
+            _delay_ms(DELAYTIME);
+            // previous LED
+            i--;
+        }
+    }
+
+    return 0;
+}
